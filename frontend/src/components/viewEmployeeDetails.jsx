5e70a992b33c14a2f2f556e38ef93663b81e0f8e
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-26 10:55:01 +0530

    comment

--- frontend/src/components/viewEmployeeDetails.jsx
+++ frontend/src/components/viewEmployeeDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 
-// this for show details about each employees
+// this for show details about each employees for hr viwe
 const ViewEmployeeDetails = () => {
     return(
         <div>Emp name
