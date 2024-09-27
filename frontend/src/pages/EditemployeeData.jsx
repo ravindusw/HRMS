d7c9fc2929af9d1dff998cad2179438d959f87f9
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-27 15:19:13 +0530

    Refactor routing and add new pages
    - Refactor the routing in App.jsx to include new pages for HR view, editing employee data, and adding new employees.
    - Add HrView.jsx, EditemployeeData.jsx, and AddEmployee.jsx to the pages directory.
    - Update ShowEmplyees.jsx to navigate to the HR view page when the "View" button is clicked.

---
+++ frontend/src/pages/EditemployeeData.jsx
@@ -0,0 +1,9 @@
+const EditemployeeData=()=>{
+    return(
+        <div>
+            <h1>Edit Employee Data</h1>
+        </div>
+    )
+
+}
+export default EditemployeeData
---
+++ frontend/src/pages/HrView.jsx
@@ -0,0 +1,9 @@
+const HrView=()=>{
+    return(
+        <div>
+            <h1>HR View</h1>
+        </div>
+    )
+
+}
+export default HrView
