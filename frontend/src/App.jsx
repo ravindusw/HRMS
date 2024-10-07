7c1c20014c5537dd7fa8979d4d77f167e9330644
Dineth Danurdha <163334443+danux2002@users.noreply.github.com>
2024-10-07 23:35:45 +0530

    leave management application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Report from './pages/Report.jsx'
 import EIM from './pages/Employee_Information_Management.jsx'
 import EditemployeeData from './pages/EditemployeeData.jsx'
 import AddEmployee from './pages/AddEmployee.jsx'
+import LeaveApplication from './pages/LeaveApplication.jsx'
 
 
 import HrView from './pages/HrView.jsx'
@@ -29,6 +30,7 @@ function App() {
           <Route path="/Employee_Information_Management/HrView/:id_to_view" element={<HrView />} />
           <Route path="/Employee_Information_Management/EditemployeeData/:id_to_edit" element={<EditemployeeData />} />
           <Route path="/Employee_Information_Management/AddEmployee" element={<AddEmployee />} />
+          <Route path="/leaveapplication" element={<LeaveApplication />} />
         </Routes>
       </BrowserRouter>
     </>
