58fbd3b1f9b92d08c662ed96dc3a3af272bdc02b
H3LLJOY <theekshanawijerathne101@gmail.com>
2024-10-09 21:10:32 +0530

    Refactor CSS to use viewport width units for image and container widths

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 // Import all routes here
-// import notificationRouter from "./routes/notification.js";
-// import getemployeesForEIMRouter from "./routes/getemployeesForEIM.js";
-// import loginRouter from "./routes/login.js";
-// import profileRouter from "./routes/profile.js";
 
 // Use the login route
-// app.use("/login", loginRouter);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
-
-// //Profile Route
-// app.use("/profile/:employee_id", profileRouter);
 
 // Start the server
 
@@ -38,20 +31,3 @@ app.use((err, req, res, next) => {
   console.error("Error message:", err.message);
   res.status(500).send("Something broke!");
 });
-
-// app.get("/profile/:employee_id", (req, res) => {
-//   const { employee_id } = req.params;
-//   const query = `SELECT  u.username,e.first_name, e.last_name, e.address, e.date_of_birth, e.email AS personal_email, e.hired_date, jt.title as job_title, d.name as Department
-//         FROM users u JOIN employees e ON u.employee_id = e.employee_id LEFT JOIN job_title jt ON e.job_title_id = jt.job_title_id  LEFT JOIN    department d ON e.dept_id = d.dept_id
-//         WHERE e.employee_id = ?`;
-//   db.query(query, [employee_id], (err, results) => {
-//     if (err) {
-//       console.error("Error fetching profile data:", err);
-//       return res.status(500).send("Server error");
-//     }
-//     if (results.length === 0) {
-//       return res.status(404).send("Profile not found");
-//     }
-//     res.status(200).json(results[0]);
-//   });
-// });
